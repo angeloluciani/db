57259371ce1f176cc0ad8b5f6b8e9b5937c161e2
Angelo Luciani <mistereal@gmail.com>
2015-12-14 16:46:26 +0100

    created useful datapump file
    created useful datapump file

---
+++ useful_data_pump_query.sql
@@ -0,0 +1,8 @@
+
+impdp <to_user>/<password>@orcl schemas=<from_dump_user> directory=DATA_PUMP_DIR dumpfile=20110728h1130_backup_db.dmp logfile=20110728h1130_backup_db.log remap_schema=<from_dump_user>:<to_user>
+
+SELECT DIRECTORY_NAME, DIRECTORY_PATH FROM DBA_DIRECTORIES WHERE DIRECTORY_NAME='DATA_PUMP_DIR';
+
+CREATE OR REPLACE DIRECTORY DATA_PUMP_DIR AS 'D:\app\oracle\admin\xdm\dpdump\';
+
+GRANT READ, WRITE ON DIRECTORY DATA_PUMP_DIR TO <to_user>; 
