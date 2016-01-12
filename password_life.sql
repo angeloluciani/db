ac30cb7ee3b104a8512a986cc70cd802ace3cda9
Angelo Luciani <mistereal@gmail.com>
2016-01-12 12:04:56 +0100

    updated
    updated

--- password_life.sql
+++ password_life.sql
@@ -2,4 +2,9 @@ select * from dba_profiles;
 
 ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED;
 
-alter user "pippo" identified by values 'pluto';
+alter user PIPPO identified by pluto;
+
+alter user PIPPO account unlock;
+
+select username,account_status from dba_users; // The account status should be open
+
