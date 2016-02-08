e7e8af20a951e45d2344e59e60a13b3e972966ab
Angelo Luciani <mistereal@gmail.com>
2016-02-08 14:43:49 +0100

    updated
    updated

--- export_db.sql
+++ export_db.sql
@@ -1,2 +1,2 @@
 exp <utente>/<password> file=/<percorso al file .dmp che sarà creato> log=/<percorso al
-file .log che sarà creato> owner=<user> consistent=yes
+file .log che sarà creato> owner=<user> consistent=yes statistics=none
