0e21da84887e58d4e6cdbe299fae98b09d0581d3
Angelo Luciani <mistereal@gmail.com>
2016-07-15 11:45:10 +0200

    added expdp
    added expdp

--- useful_data_pump_query.sql
+++ useful_data_pump_query.sql
@@ -1,4 +1,6 @@
 
+
+expdp <to_user>/<password>@orcl full=Y directory=DATA_PUMP_DIR dumpfile=15072016.dmp logfile=15072016.log
 impdp <to_user>/<password>@orcl schemas=<from_dump_user> directory=DATA_PUMP_DIR dumpfile=20110728h1130_backup_db.dmp logfile=20110728h1130_backup_db.log remap_schema=<from_dump_user>:<to_user>
 
 SELECT DIRECTORY_NAME, DIRECTORY_PATH FROM DBA_DIRECTORIES WHERE DIRECTORY_NAME='DATA_PUMP_DIR';
